ab862b59e231b69ac00e08b9ef1b9dd2d8bac903
Gyro Gyro <gyrogyro@walla.com>
2015-05-24 21:15:03 +0300

    Organizing project:  - Renaming the main GUIClient to be GasStationRemoteClientMain and moved it the the gasStation packet.
    Other changes:
     - Start adding to the ServerGUI the option for statistics.

--- Assignment2/src/gasstation/GasStationRemoteClientMain.java
+++ Assignment2/src/gasstation/GasStationRemoteClientMain.java
@@ -1,6 +1,8 @@
-package ui;
+package gasstation;
 
-public class ClientMain {
+import ui.ClientGUI;
+
+public class GasStationRemoteClientMain {
 
 	public static void main(String[] args) {
 		ClientGUI clientGUI = new ClientGUI();
--- Assignment2/src/ui/ServerGUI.java
+++ Assignment2/src/ui/ServerGUI.java
@@ -26,27 +26,35 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class ServerGUI extends Application{
-	
+public class ServerGUI extends Application {
+
 	private StackPane root;
-	private GridPane jgpRoot, jgpAddCar, jgpAddFuel, jgpStationStatus, jgpCarsStatus, jgpStatisticsHeader, jgpStatisticsTable, jgpStatisticsContainer;
-	private Label jlbAddCarTitle, jlbAddCarID, jlbAddCarWantsWash, jlbAddCarFuelAmount, jlbAddFuelTitle, jlbAddFuelAmount, jlbCloseStationTitle, jlbCarStatusTitle, jlbStatisticsTitle, jlbStatisticsService, jlbStatisticsPump;
+	private GridPane jgpRoot, jgpAddCar, jgpAddFuel, jgpStationStatus,
+			jgpCarsStatus, jgpStatisticsHeader, jgpStatisticsTable,
+			jgpStatisticsContainer;
+	private Label jlbAddCarTitle, jlbAddCarID, jlbAddCarWantsWash,
+			jlbAddCarFuelAmount, jlbAddFuelTitle, jlbAddFuelAmount,
+			jlbCloseStationTitle, jlbCarStatusTitle, jlbStatisticsTitle,
+			jlbStatisticsService, jlbStatisticsPump;
 	private TextField jtfAddCarId, jtfAddCarFuelAmount, jtfAddFuelAmount;
 	private CheckBox jchWantsWash;
-	private Button jbnAddCarAdd, jbnAddFuelAdd, jbnCloseStationStatus, jbnStatisticsRun;
-	private TableView<CarStatusRecord> jtvCarsStatus; 
+	private Button jbnAddCarAdd, jbnAddFuelAdd, jbnCloseStationStatus,
+			jbnStatisticsRun;
+	private TableView<CarStatusRecord> jtvCarsStatus;
 	private TableView jtvStatistics;
 	TableColumn<CarStatusRecord, String> jtcCarsStatusCarId;
 	TableColumn<CarStatusRecord, String> jtcCarsStatusFuel;
 	TableColumn<CarStatusRecord, String> jtcCarsStatusWash;
 	TableColumn<CarStatusRecord, String> jtcCarsStatusLeft;
-	private TableColumn<String, String> jtcStatisticDate, jtcStatisticCar, jtcStatisticAction, jtcStatisticServiceId, jtcStatisticProfit;
+	private TableColumn<String, String> jtcStatisticDate, jtcStatisticCar,
+			jtcStatisticAction, jtcStatisticServiceId, jtcStatisticProfit;
 	private ComboBox<String> jcbServiceType, jcbPump;
-	
+
 	private final static int TEXT_FIELD_MAX_WIDTH = 80;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		root = new StackPane();
 		jgpRoot = new GridPane();
 		setGridPaneSpacing(jgpRoot);
@@ -55,8 +63,8 @@ public class ServerGUI extends Application{
 		root.getChildren().add(jgpRoot);
 		primaryStage.setScene(new Scene(root, 900, 800));
 		primaryStage.setTitle("Gas Station Management System");
-	    primaryStage.show();
-		
+		primaryStage.show();
+
 	}
 
 	private void initComponents() {
@@ -65,7 +73,7 @@ public class ServerGUI extends Application{
 		initGasStationStatus();
 		initCarsStatus();
 		initStationStatistics();
-		
+
 		jgpRoot.add(jgpAddCar, 0, 0);
 		jgpRoot.add(jgpAddFuel, 1, 0);
 		jgpRoot.add(jgpStationStatus, 0, 2);
@@ -78,70 +86,81 @@ public class ServerGUI extends Application{
 		jlbStatisticsTitle.setUnderline(true);
 		jlbStatisticsService = new Label("Select Service");
 		jlbStatisticsPump = new Label("Select Pump");
-		
+
 		jbnStatisticsRun = new Button("Generate");
-		
+		jbnStatisticsRun.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				getStatistics();
+
+			}
+		});
+
 		jcbServiceType = new ComboBox<String>();
 		jcbServiceType.getItems().addAll("Wash", "Fuel");
 		jcbPump = new ComboBox<String>();
 		setListOfPumps(jcbPump);
-		
+
 		jtcStatisticDate = new TableColumn<String, String>("Datetime");
 		jtcStatisticCar = new TableColumn<String, String>("Car");
-		jtcStatisticAction = new TableColumn<String, String> ("Action Type");
-		jtcStatisticServiceId = new TableColumn<String, String> ("Service ID");
-		jtcStatisticProfit = new TableColumn<String, String>("Profit"); 
-		
+		jtcStatisticAction = new TableColumn<String, String>("Action Type");
+		jtcStatisticServiceId = new TableColumn<String, String>("Service ID");
+		jtcStatisticProfit = new TableColumn<String, String>("Profit");
+
 		jtvStatistics = new TableView<String>();
-		jtvStatistics.getColumns().setAll(jtcStatisticDate, jtcStatisticCar, jtcStatisticAction, jtcStatisticServiceId, jtcStatisticProfit);
-		
+		jtvStatistics.getColumns().setAll(jtcStatisticDate, jtcStatisticCar,
+				jtcStatisticAction, jtcStatisticServiceId, jtcStatisticProfit);
+
 		jgpStatisticsHeader = new GridPane();
 		setGridPaneSpacing(jgpStatisticsHeader);
-		
+
 		jgpStatisticsHeader.add(jlbStatisticsTitle, 0, 0);
-		jgpStatisticsHeader.add(jlbStatisticsService,0 ,1);
+		jgpStatisticsHeader.add(jlbStatisticsService, 0, 1);
 		jgpStatisticsHeader.add(jcbServiceType, 1, 1);
 		jgpStatisticsHeader.add(jlbStatisticsPump, 2, 1);
 		jgpStatisticsHeader.add(jcbPump, 3, 1);
 		jgpStatisticsHeader.add(jbnStatisticsRun, 4, 1);
-		
+
 		jgpStatisticsTable = new GridPane();
 		setGridPaneSpacing(jgpStatisticsTable);
-		
+
 		jgpStatisticsTable.add(jtvStatistics, 0, 0);
-		
+
 		jgpStatisticsContainer = new GridPane();
-	
+
 		jgpStatisticsContainer.add(jgpStatisticsHeader, 0, 0);
 		jgpStatisticsContainer.add(jgpStatisticsTable, 0, 1);
 	}
 
 	private void setListOfPumps(ComboBox<String> jcbPump) {
 		// TODO Set list of pumps
-		
+
 	}
 
 	private void initCarsStatus() {
 		jlbCarStatusTitle = new Label("Cars Statuses");
 		jlbCarStatusTitle.setUnderline(true);
-			
+
 		jtcCarsStatusCarId = new TableColumn<CarStatusRecord, String>("Car");
 		jtcCarsStatusFuel = new TableColumn<CarStatusRecord, String>("Fueling");
 		jtcCarsStatusWash = new TableColumn<CarStatusRecord, String>("Washing");
 		jtcCarsStatusLeft = new TableColumn<CarStatusRecord, String>("Left");
-		
+
 		jtvCarsStatus = new TableView<CarStatusRecord>();
-		jtvCarsStatus.getColumns().addAll(jtcCarsStatusCarId, jtcCarsStatusFuel, jtcCarsStatusWash, jtcCarsStatusLeft);
-		
+		jtvCarsStatus.getColumns().addAll(jtcCarsStatusCarId,
+				jtcCarsStatusFuel, jtcCarsStatusWash, jtcCarsStatusLeft);
+
 		jgpCarsStatus = new GridPane();
 		setGridPaneSpacing(jgpCarsStatus);
-		
+
 		jgpCarsStatus.add(jlbCarStatusTitle, 0, 0);
-		//blank gridpane in order to align te content of the screen
+		// blank gridpane in order to align te content of the screen
 		GridPane jgpBlank = new GridPane();
-		jgpBlank.add(new Label(""),0 ,0);
+		jgpBlank.add(new Label(""), 0, 0);
 		setGridPaneSpacing(jgpBlank);
-		
+
 		jgpCarsStatus.add(jgpBlank, 0, 1);
 		jgpCarsStatus.add(jtvCarsStatus, 0, 2);
 	}
@@ -149,61 +168,61 @@ public class ServerGUI extends Application{
 	private void initGasStationStatus() {
 		jlbCloseStationTitle = new Label("Update Station Status");
 		jlbCloseStationTitle.setUnderline(true);
-		
+
 		jbnCloseStationStatus = new Button("Close");
-		
+
 		jgpStationStatus = new GridPane();
 		setGridPaneSpacing(jgpStationStatus);
-		
-		jgpStationStatus.add(jlbCloseStationTitle,0 ,0);
-		jgpStationStatus.add(jbnCloseStationStatus,0 ,1);
+
+		jgpStationStatus.add(jlbCloseStationTitle, 0, 0);
+		jgpStationStatus.add(jbnCloseStationStatus, 0, 1);
 	}
 
 	private void initAddFuelAmountSection() {
 		jlbAddFuelAmount = new Label("Fuel Amount");
 		jlbAddFuelTitle = new Label("Add Fuel to Main Pool");
 		jlbAddFuelTitle.setUnderline(true);
-		
+
 		jtfAddFuelAmount = new TextField("0");
 		jtfAddFuelAmount.setMaxWidth(TEXT_FIELD_MAX_WIDTH);
-		
+
 		jbnAddFuelAdd = new Button("Add");
-		
+
 		jgpAddFuel = new GridPane();
 		setGridPaneSpacing(jgpAddFuel);
-		
+
 		jgpAddFuel.add(jlbAddFuelTitle, 0, 0);
 		jgpAddFuel.add(jlbAddFuelAmount, 0, 1);
 		jgpAddFuel.add(jtfAddFuelAmount, 1, 1);
-		jgpAddFuel.add(jbnAddFuelAdd, 0, 2);	
+		jgpAddFuel.add(jbnAddFuelAdd, 0, 2);
 	}
 
 	private void initAddCarSection() {
-		
+
 		jlbAddCarTitle = new Label("Add Car to Gas Station");
 		jlbAddCarTitle.setUnderline(true);
 		jlbAddCarID = new Label("Car ID");
 		jlbAddCarWantsWash = new Label("Wants Wash");
 		jlbAddCarFuelAmount = new Label("Fuel Amount");
-		
+
 		jtfAddCarId = new TextField();
 		jtfAddCarId.setMaxWidth(TEXT_FIELD_MAX_WIDTH);
 		jtfAddCarFuelAmount = new TextField("0");
 		jtfAddCarFuelAmount.setMaxWidth(TEXT_FIELD_MAX_WIDTH);
-		
+
 		jchWantsWash = new CheckBox();
 		jchWantsWash.setSelected(true);
-		
+
 		jbnAddCarAdd = new Button("Add");
-		
+
 		jbnAddCarAdd.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			@Override
 			public void handle(ActionEvent arg0) {
-				addCarToStation();	
+				addCarToStation();
 			}
 		});
-		
+
 		jgpAddCar = new GridPane();
 		setGridPaneSpacing(jgpAddCar);
 
@@ -215,52 +234,55 @@ public class ServerGUI extends Application{
 		jgpAddCar.add(jtfAddCarId, 1, 1);
 		jgpAddCar.add(jtfAddCarFuelAmount, 1, 3);
 		jgpAddCar.add(jbnAddCarAdd, 0, 4);
-		
-
 	}
 
 	protected void addCarToStation() {
 		int carId;
 		boolean requiredWash;
 		boolean requiredFuel;
-		int fuelAmount=0;
-		
-		try{
+		int fuelAmount = 0;
+
+		try {
 			carId = Integer.parseInt(jtfAddCarId.getText());
 			requiredWash = jchWantsWash.isSelected();
-			requiredFuel = Integer.parseInt(jtfAddCarFuelAmount.getText()) == 0 ? false : true ;
+			requiredFuel = Integer.parseInt(jtfAddCarFuelAmount.getText()) == 0 ? false
+					: true;
 			fuelAmount = Integer.parseInt(jtfAddCarFuelAmount.getText());
 			updateCarStatus();
-			//ServerController.getServerController().addCar(carId, requiredFuel, fuelAmount, requiredWash);
-			
-			//clear form
+			// ServerController.getServerController().addCar(carId,
+			// requiredFuel, fuelAmount, requiredWash);
+
+			// clear form
 			jtfAddCarId.setText("");
 			jchWantsWash.setSelected(true);
 			jtfAddFuelAmount.setText("0");
-			
-		}catch (Exception e){
-			//TODO:invalid input;
+
+		} catch (Exception e) {
+			// TODO:invalid input;
 			System.out.println(e);
-		}		
-		
+		}
+
 	}
 
-	public void updateCarStatus(){
+	public void updateCarStatus() {
 		ObservableList<Object> data = FXCollections.observableArrayList();
 		CarStatusRecord tblRecord = new CarStatusRecord("1", "2", "3", "4");
-		
+
 		data.add(tblRecord);
-		jtvCarsStatus.setItems(data);
+		// jtvCarsStatus.setItems(data);
 	}
-	
-	private void setGridPaneSpacing(GridPane gp){
+
+	private void setGridPaneSpacing(GridPane gp) {
 		gp.setPadding(new Insets(5));
 		gp.setHgap(10);
 		gp.setVgap(10);
 	}
-	
+
+	private void getStatistics() {
+		ServerController.getServerController().getStatistics("Fuel", "1");
+	}
+
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 }
