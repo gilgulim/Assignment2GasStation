4ba5a30d4448a134f4c566fcd11a3692b1d430b2
Gil Peretz <gilperetz2002@gmail.com>
2015-06-14 20:27:40 +0300

    added GasStationJPAManager and implemented insertCar(CarObject) method. added dataObject Package to GasStationJPADAL project
    Ready to check POC now

--- GasStationJPADAL/src/dalJPA/GasStationJPAManager.java
+++ GasStationJPADAL/src/dalJPA/GasStationJPAManager.java
@@ -4,27 +4,34 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import dataObjects.CarObject;
+
 public class GasStationJPAManager {
 
-	public static void main(String[] args) {
-		Car car = new Car();
-		car.setCarID(999);
-		car.setIsCarReqFuel((byte)1);
-		car.setIsCarReqWash((byte)1);
-
+	public boolean insertCar(CarObject carObject){
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("GasStationJPADAL");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		
-		try{
+		try {
+			Car car = new Car();
+			
+			car.setCarID(carObject.getId());
+			car.setCarFuelQuantity((carObject.getNumOfLiters() != 0 ? carObject.getNumOfLiters() : null));	
+			car.setIsCarReqFuel((carObject.getWantFuel() ? (byte) 1 : 0));
+			car.setIsCarReqWash((carObject.getWantCleaning() ? (byte) 1 : 0));
+
+			em.getTransaction().begin();
 			em.persist(car);
 			em.getTransaction().commit();
-		}catch (Exception e){
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
 			em.getTransaction().rollback();
+			return false;
 		}finally{
 			em.close();
 		}
 	}
-
+	
+	
 }
