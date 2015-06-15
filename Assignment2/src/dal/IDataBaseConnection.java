1fa8d9f1c18933973c8584fe9032dfc26714c0b3
Gyro Gyro <gyrogyro@walla.com>
2015-06-15 21:48:35 +0300

    Fixing IDataBaseConnection interface.

--- Assignment2/src/dal/IDataBaseConnection.java
+++ Assignment2/src/dal/IDataBaseConnection.java
@@ -9,15 +9,15 @@ import dal.dataObjects.GasStationHistoryRecord.ActionType;
 
 public interface IDataBaseConnection {
 		
-	public abstract boolean clearDatabase();
-	public abstract boolean insertGasStationHistoryRecord(GasStationHistoryRecord historyRecord);
-	public abstract boolean insertCar(CarObject car);
-	public abstract boolean insertGasStation(GasStationObject gasStation);
-	public abstract GasStationObject getGasStationById(int gasStationId);
-	public abstract int getLittersByCarId(int carId);
-	public abstract int getPricePerLittersByPumpId(int pumpId);
-	public abstract int getNumberOfPumps();
-	public abstract ArrayList<GasStationHistoryRecord> getStatistics(ActionType actionType, int serviceId);
+	boolean clearDatabase();
+	boolean insertGasStationHistoryRecord(GasStationHistoryRecord historyRecord);
+	boolean insertCar(CarObject car);
+	boolean insertGasStation(GasStationObject gasStation);
+	GasStationObject getGasStationById(int gasStationId);
+	int getLittersByCarId(int carId);
+	int getPricePerLittersByPumpId(int pumpId);
+	int getNumberOfPumps();
+	ArrayList<GasStationHistoryRecord> getStatistics(ActionType actionType, int serviceId);
 	
 	 
 }
